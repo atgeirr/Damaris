47e2105ba84ee9c3cb2f790d6ba5a5421eda13c9
mdorier <mdorier>
2011-06-09 16:40:02 +0000

    release candidate, need to test and document

--- trunk/plugins/template.cpp
+++ trunk/plugins/template.cpp
@@ -5,7 +5,7 @@ extern "C" {
 
 void my_function(const std::string* event, int32_t step, int32_t src, Damaris::MetadataManager* db)
 {
-	printf("hello world");
+	printf("--- hello world from Damaris ---\n");
 }
 
 }
