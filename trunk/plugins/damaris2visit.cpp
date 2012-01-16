3d6ac68d0985f16d74d587d78e434a91f59db3aa
mdorier <mdorier>
2012-01-16 10:54:55 +0000

    modifier templates

--- trunk/plugins/damaris2visit.cpp
+++ trunk/plugins/damaris2visit.cpp
@@ -2,7 +2,7 @@
 
 extern "C" {
 
-void my_function(const std::string event, int32_t step, int32_t src)
+void visit(const std::string event, int32_t step, int32_t src)
 {
 	printf("--- hello world from Damaris ---\n");
 }
