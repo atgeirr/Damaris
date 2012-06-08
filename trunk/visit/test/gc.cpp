bfc17238a3a48730e5c8e359bac42f3f660ed8bc
Matthieu Dorier <matthieu.dorier@irisa.fr>
2012-06-08 22:44:41 +0200

    finding a segfault... actually located in VisIt

---
+++ trunk/visit/test/gc.cpp
@@ -0,0 +1,24 @@
+#include "core/Debug.hpp"
+#include "core/VariableManager.hpp"
+
+using namespace Damaris;
+
+extern "C" {
+
+void internal_gc(const std::string& event, int32_t step, int32_t src)
+{
+	int x = 0;
+	Variable* v = VariableManager::Search("life/cells");
+	if(v != NULL) {
+		ChunkIndexByIteration::iterator end;
+		ChunkIndexByIteration::iterator c = v->getChunksByIteration(step, end);
+		while(c != end) {
+			v->detachChunk(c->get());
+			x++;
+			c++;
+		}
+	}
+	INFO("successfully deleted " << x << " chunks");
+}
+
+}
