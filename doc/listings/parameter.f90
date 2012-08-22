53f899aa439863e9ec5e32a0ef259e3fff66bb39
Matthieu Dorier <matthieu.dorier@irisa.fr>
2012-08-22 13:05:08 -0500

    corrected API in parameter

--- doc/listings/parameter.f90
+++ doc/listings/parameter.f90
@@ -1,7 +1,7 @@
 integer :: w_in, w_out
 integer :: ierr
 ...
-call df_parameter_get("w",w_in,ierr)
+call df_parameter_get("w",w_in,SIZEOF(w_in),ierr)
 
 w_out = 4
-call df_parameter_set("w",w_out,ierr)
+call df_parameter_set("w",w_out,SIZEOF(w_out),ierr)
