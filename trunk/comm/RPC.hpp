442ebdd6603a5c75d8df9bb8d8de45537177aa71
Matthieu Dorier <matthieu.dorier@irisa.fr>
2013-04-27 03:40:45 +0200

    added RPC.hpp

---
+++ trunk/comm/RPC.hpp
@@ -0,0 +1,36 @@
+/*******************************************************************
+This file is part of Damaris.
+
+Damaris is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Damaris is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Damaris.  If not, see <http://www.gnu.org/licenses/>.
+********************************************************************/
+/**
+ * \file RPC.hpp
+ * \date February 2013
+ * \author Matthieu Dorier
+ * \version 0.7
+ */
+
+#ifndef __DAMARIS_RPC_H
+#define __DAMARIS_RPC_H
+
+namespace Damaris {
+
+enum RPC {
+	RPC_VISIT_CONNECTED,
+	RPC_VISIT_UPDATE
+};
+
+}
+
+#endif
