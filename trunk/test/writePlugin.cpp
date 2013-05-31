62fdcc175cc5ef29e7f48a16370812505729c249
cata <cata@cata-Aspire-5735.(none)>
2013-05-31 18:28:00 +0200

    WritePlugin

---
+++ trunk/test/writePlugin.cpp
@@ -0,0 +1,22 @@
+
+#include <stdint.h>
+#include <iostream>
+#include <string>
+
+#include "core/VariableManager.hpp"
+#include "storage/Writer.h"
+#include "storage/StorageManager.h"
+
+
+extern "C" {
+void writeVar(const std::string& eventName, int32_t step, 
+	int32_t source, const char* args) {
+	
+        Damaris::Variable* v = Damaris::VariableManager::Search("my_group/my_variable");
+	Damaris::Writer* w = Damaris::StorageManager::GetWriterFor(v);      
+        w->Write();
+       
+      
+}
+}
+
