0d894f9d7d9add2be50853475f0ce09d85751e95
Matthieu Dorier <mdorier@anl.gov>
2015-09-23 12:54:11 -0500

    added the possibility to compile Damaris without the XSD binary if the model has already been generated

--- trunk/cmake/Modules/FindVisIt.cmake
+++ trunk/cmake/Modules/FindVisIt.cmake
@@ -4,6 +4,7 @@
 #  VisIt_FOUND - VisIt is installed
 #  VisIt_INCLUDE_DIRS - Directories where VisIt headers can be found
 #  VisIt_LIBRARIES - Libraries required by VisIt headers
+include(FindPackageHandleStandardArgs)
 
 set(VisIt_VERSIONS 2.9.0 2.8.3 2.8.2 2.8.1 2.8.0 2.7.3 2.7.2 2.7.1 2.7.0 2.6.3 2.6.2 2.6.1 2.6.0 2.5.2)
 set(VisIt_ARCHITECTURES linux-x86_64)
--- trunk/cmake/Modules/FindXSD.cmake
+++ trunk/cmake/Modules/FindXSD.cmake
@@ -5,6 +5,7 @@
 #
 FIND_PROGRAM( XSD_EXECUTABLE xsd
 		   	  HINTS ${RWSL_DEPS}/xsd/bin $ENV{XSD_ROOT}/bin
+			  ${XSD_ROOT}/bin
 			  PATHS /usr/local/xsd-3.2.0-i686-macosx/bin
 			  		/usr/local/xsd-3.2.0-x86_64-linux-gnu/bin
 			  		/usr/local/bin
