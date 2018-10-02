032b31f77b5d6fe38a845062eac7e6fbea2c240d
Matthieu Dorier <matthieu.dorier@irisa.fr>
2018-10-02 21:24:01 +0200

    better pc file generation

--- lib/make-pc-file.sh
+++ lib/make-pc-file.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed -e "s/PV_LIBS/$(<pv-config.out.2 sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" -i damaris.pc
+sed -e "s/PV_LIBS/$(<pv-config.out.2 sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' | tr -d '\n')/g" damaris.pc.in > damaris.pc
