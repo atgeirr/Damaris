fe0f0d1701087f6ec927bd1b6d978707775a58db
josh bowden <joshua-charles.bowden@inria.fr>
2022-07-15 08:16:19 +0200

    enhanced test of streaming stats class

--- examples/python/dask_online_stats.py
+++ examples/python/dask_online_stats.py
@@ -1,108 +1,25 @@
 #!/usr/bin/python3
 # Author: Josh Bowden, Inria
-# Description: Part of the Damaris examples of using Python integration with Dask distributed
-# To run this example, a Dask scheduler needs to be spun up:
+# Usage:
+# > ./dask_online_stats <float> | -u | -s | -r |
+# <float> a value to add to the data
+#  -u     Unpublish datasets 
+#  -s     Shutdown dask scheduler 
+#  -r    [num] Start Dask Scheduler with num workers (default 2)  
+#
+# Description: 
+#  Test code for streaming statistics using Dask, based on Welford's online algorithm.
+#  (https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#:~:text=to%20a%20degree.-,Welford%27s%20online%20algorithm,-%5Bedit%5D)
+#
+#  Part of the Damaris examples of using Python integration with Dask distributed
+#  To run this example, a Dask scheduler needs to be spun up:
 # 
-#   dask-scheduler --scheduler-file "/home/$USER/dask_file.json" &
-#   dask-worker --scheduler-file "/home/$USER/dask_file.json"  --nthreads 1 &
 #
 
 import dask.array as da
 import numpy as np
-from dask.distributed import Client, Lock, Variable
+from dask.distributed import Client
 import sys
-"""
-
-def create(firstValue):
-        count = 0
-        mean = da.full_like(firstValue, 0.0)
-        M2   = da.full_like(firstValue, 0.0)
-        return (count, mean, M2)
-
-    # For a new value newValue, compute the new count, new mean, the new M2.
-    # mean accumulates the mean of the entire dataset
-    # M2 aggregates the squared distance from the mean
-    # count aggregates the number of samples seen so far
-    def update(existingAggregate, newValue):
-        (count, mean, M2) = existingAggregate
-        count += 1
-        delta = newValue - mean
-        mean += delta / count
-        delta2 = newValue - mean
-        M2 += delta * delta2
-        return (count, mean, M2)
-
-    # Retrieve the mean, variance and sample variance from an aggregate
-    def finalize(existingAggregate):
-        (count, mean, M2) = existingAggregate
-        if count < 2:
-            return float("nan")
-        else:
-            (mean, sampleVariance) = (mean, M2 / (count - 1))
-            return (mean, sampleVariance)
-            
-    def compute_block_average(block):  
-        return np.array([np.average(block)])[:, None, None]
-
-    def compute_block_average_of_sqrt(block):  
-        return np.array([np.average(np.sqrt(block))])[:, None, None]
-        
-# Testing
----------    
-block_shape = (1, 3, 4)
-
-P0_B0 = np.zeros(block_shape )
-P1_B0 = np.ones(block_shape  )
-P2_B0 = np.ones(block_shape)*2
-P3_B0 = np.ones(block_shape)*3
-P4_B0 = np.ones(block_shape)*4
-P5_B0 = np.ones(block_shape)*5
-P6_B0 = np.ones(block_shape)*6
-P7_B0 = np.ones(block_shape)*7
-P8_B0 = np.ones(block_shape)*8
-P9_B0 = np.ones(block_shape)*9
-P10_B0 = np.ones(block_shape)*10
-P11_B0 = np.ones(block_shape)*11
-P12_B0 = np.ones(block_shape)*12
-P13_B0 = np.ones(block_shape)*13
-P14_B0 = np.ones(block_shape)*14
-P15_B0 = np.ones(block_shape)*15
-
-data = [
-    [
-        [P0_B0,  P1_B0,  P2_B0,  P3_B0],
-        [P4_B0,  P5_B0,  P6_B0,  P7_B0],
-        [P8_B0,  P9_B0,  P10_B0, P11_B0],
-        [P12_B0, P13_B0, P14_B0, P15_B0]
-    ]
-]
-data2 = [
-    [
-        [P1_B0,  P1_B0,  P1_B0,  P1_B0],
-        [P1_B0,  P1_B0,  P1_B0,  P1_B0],
-        [P1_B0,  P1_B0,  P1_B0,  P1_B0],
-        [P1_B0,  P1_B0,  P1_B0,  P1_B0]
-    ]
-]
-
-x = da.block(data)
-
-print(x.compute())
-print(da.average(x).compute())
-existingAggregate = create(x)
-
-for i in range(100):
-    y = x + da.random.normal(0.0, 0.1, x.shape)
-    existingAggregate = update(existingAggregate, y)
-
-(mean, sampleVariance) = finalize(existingAggregate)
-
-print(mean.map_blocks(compute_block_average, chunks=(1, 1, 1)).compute())
-print(sampleVariance.map_blocks(compute_block_average_of_sqrt, chunks=(1, 1, 1)).compute())
-print(da.average(da.sqrt(sampleVariance)).compute()))
-            """
-# from dask.distributed import Client
-# client =  Client(scheduler_file='/home/jbowden/dask_file.json', timeout='2s')
 
 
 class DaskStats:
@@ -150,11 +67,13 @@ class DaskStats:
         
         self.count += 1
         delta = newValue - self.mean
+        delta.persist()
         self.mean += delta / self.count
+        self.mean.persist()
         delta2 = newValue - self.mean
         self.M2 += delta * delta2
         
-        self.mean.persist()
+        # self.mean.persist()
         self.M2.persist()
         
         
@@ -257,11 +176,11 @@ class DaskStats:
         Result:
         End up with a Numpy array of <number of chunks in the dask array> values
         """
-        # shape_len = len(block.shape)
-        # global res 
-        # exec_str = 'global res; res = np.array([np.average(block)])' + self.get_array_as_string(shape_len)
-        # exec(exec_str)
-        res = np.array([np.average(block)])[:, None, None]
+        shape_len = len(block.shape)
+        global res 
+        exec_str = 'global res; res = np.array([np.average(block)])' + self.get_array_as_string(shape_len)
+        exec(exec_str)
+        # res = np.array([np.average(block)])[:, None, None]
         return(res)
         
 
@@ -275,11 +194,11 @@ class DaskStats:
         End up with a Numpy array of <number of chunks in the dask array> values,
         each value is the ave(sqrt(original data)), sqrt() is per element and ave() is a reduction.
         """
-        # shape_len = len(block.shape)
-        # global res 
-        # exec_str = 'global res; res = np.array([np.average(np.sqrt(block))])' + self.get_array_as_string(shape_len)
-        # exec(exec_str)
-        res = np.array([np.average(np.sqrt(block))])[:, None, None]
+        shape_len = len(block.shape)
+        global res 
+        exec_str = 'global res; res = np.array([np.average(np.sqrt(block))])' + self.get_array_as_string(shape_len)
+        exec(exec_str)
+        # res = np.array([np.average(np.sqrt(block))])[:, None, None]
         return(res)
     
     
@@ -355,14 +274,23 @@ data = [
 
 x = da.block(data)
 
+def compute_block_average(block):  
+    return np.array([np.average(block)])[:, None, None]
 
+def compute_block_average_of_sqrt(block):  
+    return np.array([np.average(np.sqrt(block))])[:, None, None]
 
 # from dask.distributed import Client
-client =  Client(scheduler_file='/home/jbowden/dask_file.json', timeout='2s')
+def print_help()
+    print(sys.argv[1], '  <float> ')
+    print(sys.argv[1], '  -u  Unpublish datasets ')
+    print(sys.argv[1], '  -s  Shutdown dask scheduler ')
+    print(sys.argv[1], '  -r  [num] Restart Dask Scaheduler with num workers (default 2)  ')
 
-print(client.list_datasets())
 unpublish = False
 shutdown = False
+restart = False
+how_many_workers = 2
 if len(sys. argv) > 1: 
     if (sys.argv[1] == '-u'):
         print('unpublish = True')
@@ -370,10 +298,33 @@ if len(sys. argv) > 1:
     elif (sys.argv[1] == '-s'):
         print('shutdown = True')
         shutdown = True
+    elif (sys.argv[1] == '-r'):
+        print('restart = True')
+        restart = True
+        if len(sys. argv) > 2:
+            how_many_workers = int(sys.argv[2])
+    elif (sys.argv[1] == '-h'):
+        print_help()
+        sys.exit()
     else:
         myval = float(sys.argv[1])
 else:
-    myval = 1.0 
+    print_help() 
+
+
+    
+if restart == True :
+    import subprocess, time
+    print('Restarting down the Dask Scheduler and workers!')
+    #client.shutdown()
+    subprocess.Popen(['dask-scheduler', '--scheduler-file', '/home/jbowden/dask_file.json'])
+    time.sleep(1.0)
+    # dask-scheduler --scheduler-file "/home/$USER/dask_file.json" &
+    for i in range(how_many_workers):
+        subprocess.Popen(['dask-worker', '--scheduler-file', '/home/jbowden/dask_file.json',  '--nthreads', '1'])
+
+client =  Client(scheduler_file='/home/jbowden/dask_file.json', timeout='2s')
+print(client.list_datasets())
 
 if unpublish == True :
     for ds  in client.list_datasets():
@@ -386,9 +337,8 @@ if shutdown == True :
     print('Shuting down the Dask Scheduler and workers!')
     client.shutdown()
     sys.exit()
-
-
-
+    
+    
 if 'unique_name1' not in client.list_datasets():
     print('Creating Daskstats:')
     daskstats = DaskStats(x)
@@ -405,16 +355,12 @@ for i in range(10):
 
 
 print('The current mean and stddev values are:')
-# (mean, sampleVariance) = daskstats.return_mean_var_tuple()
-mean           = daskstats.return_mean()
-sampleVariance = daskstats.return_variance()
-
-mean.persist()
-sampleVariance.persist()
+(mean, sampleVariance) = daskstats.return_mean_var_tuple()
 
 my_tuple = daskstats.get_chunks(x)  # tuple of 1's e.g. (1, 1, 1)
-print(mean.map_blocks(daskstats.compute_block_average, chunks=my_tuple).compute())
+print(mean.map_blocks(daskstats.compute_block_average, chunks=(1, 1, 1)).compute())
 print(sampleVariance.map_blocks(daskstats.compute_block_average_of_sqrt, chunks=my_tuple).compute())
+
 # Compare with global derived value
 print(da.average(da.sqrt(sampleVariance)).compute())
 
