421c8b29add63fbfce3d1211799858ec23ea0db9
josh bowden <joshua-charles.bowden@inria.fr>
2022-07-26 11:45:30 +0200

    updated version numbers were not pushed to damaris-development, so updating now

--- build/spack/repo/packages/damaris/package.py
+++ build/spack/repo/packages/damaris/package.py
@@ -16,6 +16,7 @@ class Damaris(CMakePackage):
     maintainers = ['jcbowden']
 
     version('master', branch='master')
+    version('1.6.0',  tag='v1.7.0')
     version('1.6.0',  tag='v1.6.0')
     version('1.5.0',  tag='v1.5.0')
     version('1.3.3',  tag='v1.3.3')
